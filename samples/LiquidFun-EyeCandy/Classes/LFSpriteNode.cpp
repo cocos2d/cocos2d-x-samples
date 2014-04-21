ea39f4afca8ce92e07e6494afd616d26be6f1e68
Ricardo Quesada <ricardoquesada@gmail.com>
2014-04-20 21:39:25 -0700

    Adds LHSprite

---
+++ samples/LiquidFun-EyeCandy/Classes/LFSpriteNode.cpp
@@ -0,0 +1,295 @@
+/* Copyright (c) 2012 Scott Lembcke and Howling Moon Software
+ * Copyright (c) 2012 cocos2d-x.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "LFSpriteNode.h"
+
+using namespace cocos2d;
+
+LFSpriteNode::LFSpriteNode()
+: _ignoreBodyRotation(false)
+, _pB2Body(nullptr)
+, _PTMRatio(0.0f)
+{}
+
+LFSpriteNode* LFSpriteNode::create()
+{
+    LFSpriteNode* pRet = new LFSpriteNode();
+    if (pRet && pRet->init())
+    {
+        pRet->autorelease();
+    }
+    else
+    {
+        CC_SAFE_DELETE(pRet);
+    }
+
+    return pRet;
+}
+
+LFSpriteNode* LFSpriteNode::createWithTexture(Texture2D *pTexture)
+{
+    LFSpriteNode* pRet = new LFSpriteNode();
+    if (pRet && pRet->initWithTexture(pTexture))
+    {
+        pRet->autorelease();
+    }
+    else
+    {
+        CC_SAFE_DELETE(pRet);
+    }
+
+    return pRet;
+}
+
+LFSpriteNode* LFSpriteNode::createWithTexture(Texture2D *pTexture, const Rect& rect)
+{
+    LFSpriteNode* pRet = new LFSpriteNode();
+    if (pRet && pRet->initWithTexture(pTexture, rect))
+    {
+        pRet->autorelease();
+    }
+    else
+    {
+        CC_SAFE_DELETE(pRet);
+    }
+
+    return pRet;
+}
+
+LFSpriteNode* LFSpriteNode::createWithSpriteFrame(SpriteFrame *pSpriteFrame)
+{
+    LFSpriteNode* pRet = new LFSpriteNode();
+    if (pRet && pRet->initWithSpriteFrame(pSpriteFrame))
+    {
+        pRet->autorelease();
+    }
+    else
+    {
+        CC_SAFE_DELETE(pRet);
+    }
+
+    return pRet;
+}
+
+LFSpriteNode* LFSpriteNode::createWithSpriteFrameName(const char *pszSpriteFrameName)
+{
+    LFSpriteNode* pRet = new LFSpriteNode();
+    if (pRet && pRet->initWithSpriteFrameName(pszSpriteFrameName))
+    {
+        pRet->autorelease();
+    }
+    else
+    {
+        CC_SAFE_DELETE(pRet);
+    }
+
+    return pRet;
+}
+
+LFSpriteNode* LFSpriteNode::create(const char *pszFileName)
+{
+    LFSpriteNode* pRet = new LFSpriteNode();
+    if (pRet && pRet->initWithFile(pszFileName))
+    {
+        pRet->autorelease();
+    }
+    else
+    {
+        CC_SAFE_DELETE(pRet);
+    }
+
+    return pRet;
+}
+
+LFSpriteNode* LFSpriteNode::create(const char *pszFileName, const Rect& rect)
+{
+    LFSpriteNode* pRet = new LFSpriteNode();
+    if (pRet && pRet->initWithFile(pszFileName, rect))
+    {
+        pRet->autorelease();
+    }
+    else
+    {
+        CC_SAFE_DELETE(pRet);
+    }
+
+    return pRet;
+}
+
+// this method will only get called if the sprite is batched.
+// return YES if the physic's values (angles, position ) changed.
+// If you return NO, then getNodeToParentTransform won't be called.
+bool LFSpriteNode::isDirty() const
+{
+    return true;
+}
+
+bool LFSpriteNode::isIgnoreBodyRotation() const
+{
+    return _ignoreBodyRotation;
+}
+
+void LFSpriteNode::setIgnoreBodyRotation(bool bIgnoreBodyRotation)
+{
+    _ignoreBodyRotation = bIgnoreBodyRotation;
+}
+
+// Override the setters and getters to always reflect the body's properties.
+const Point& LFSpriteNode::getPosition() const
+{
+    return getPosFromPhysics();
+}
+
+void LFSpriteNode::getPosition(float* x, float* y) const
+{
+    if (x == NULL || y == NULL) {
+        return;
+    }
+    const Point& pos = getPosFromPhysics();
+    *x = pos.x;
+    *y = pos.y;
+}
+
+float LFSpriteNode::getPositionX() const
+{
+    return getPosFromPhysics().x;
+}
+
+float LFSpriteNode::getPositionY() const
+{
+    return getPosFromPhysics().y;
+}
+
+//
+// Chipmunk only
+//
+
+
+
+b2Body* LFSpriteNode::getB2Body() const
+{
+    return _pB2Body;
+}
+
+void LFSpriteNode::setB2Body(b2Body *pBody)
+{
+    _pB2Body = pBody;
+}
+
+float LFSpriteNode::getPTMRatio() const
+{
+    return _PTMRatio;
+}
+
+void LFSpriteNode::setPTMRatio(float fRatio)
+{
+    _PTMRatio = fRatio;
+}
+
+//
+// Common to Box2d and Chipmunk
+//
+
+const Point& LFSpriteNode::getPosFromPhysics() const
+{
+    static Point s_physicPosion;
+
+    b2Vec2 pos = _pB2Body->GetPosition();
+    float x = pos.x * _PTMRatio;
+    float y = pos.y * _PTMRatio;
+    s_physicPosion = Point(x,y);
+    return s_physicPosion;
+}
+
+void LFSpriteNode::setPosition(const Point &pos)
+{
+    float angle = _pB2Body->GetAngle();
+    _pB2Body->SetTransform(b2Vec2(pos.x / _PTMRatio, pos.y / _PTMRatio), angle);
+}
+
+float LFSpriteNode::getRotation() const
+{
+    return (_ignoreBodyRotation ? Sprite::getRotation() :
+            CC_RADIANS_TO_DEGREES(_pB2Body->GetAngle()));
+}
+
+void LFSpriteNode::setRotation(float fRotation)
+{
+    if (_ignoreBodyRotation)
+    {
+        Sprite::setRotation(fRotation);
+    }
+    else
+    {
+        b2Vec2 p = _pB2Body->GetPosition();
+        float radians = CC_DEGREES_TO_RADIANS(fRotation);
+        _pB2Body->SetTransform(p, radians);
+    }
+}
+
+void LFSpriteNode::visit(Renderer *renderer, const kmMat4& parentTransform, bool parentTransformUpdated)
+{
+    // box2d controls the position, so set the transform as dirty
+    _transformUpdated = true;
+    Sprite::visit(renderer, parentTransform, parentTransformUpdated);
+}
+
+// returns the transform matrix according the Chipmunk Body values
+const kmMat4& LFSpriteNode::getNodeToParentTransform() const
+{
+    // Although scale is not used by physics engines, it is calculated just in case
+	// the sprite is animated (scaled up/down) using actions.
+	// For more info see: http://www.cocos2d-iphone.org/forum/topic/68990
+
+    b2Vec2 pos  = _pB2Body->GetPosition();
+
+	float x = pos.x * _PTMRatio;
+	float y = pos.y * _PTMRatio;
+
+	if (_ignoreAnchorPointForPosition)
+    {
+		x += _anchorPointInPoints.x;
+		y += _anchorPointInPoints.y;
+	}
+
+	// Make matrix
+	float radians = _pB2Body->GetAngle();
+	float c = cosf(radians);
+	float s = sinf(radians);
+
+	if (!_anchorPointInPoints.equals(Point::ZERO))
+    {
+		x += ((c * -_anchorPointInPoints.x * _scaleX) + (-s * -_anchorPointInPoints.y * _scaleY));
+		y += ((s * -_anchorPointInPoints.x * _scaleX) + (c * -_anchorPointInPoints.y * _scaleY));
+	}
+
+	// Rot, Translate Matrix
+
+    kmScalar mat[] = {  (kmScalar)c * _scaleX, (kmScalar)s * _scaleX, 0,  0,
+        (kmScalar)-s * _scaleY, (kmScalar)c * _scaleY,  0,  0,
+        0,  0,  1,  0,
+        x,	y,  0,  1};
+
+
+    kmMat4Fill(&_transform, mat);
+	return _transform;
+}
---
+++ samples/LiquidFun-EyeCandy/Classes/LFSpriteNode.h
@@ -0,0 +1,118 @@
+/* Copyright (c) 2012 Scott Lembcke and Howling Moon Software
+ * Copyright (c) 2012 cocos2d-x.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef __LFSPRITENODE_H__
+#define __LFSPRITENODE_H__
+
+#include "cocos2d.h"
+#include <Box2D/Box2D.h>
+
+/** A Sprite subclass that is bound to a physics body.
+ It works with:
+ - Chipmunk: Preprocessor macro CC_ENABLE_CHIPMUNK_INTEGRATION should be defined
+ - Objective-Chipmunk: Preprocessor macro CC_ENABLE_CHIPMUNK_INTEGRATION should be defined
+ - Box2d: Preprocessor macro CC_ENABLE_BOX2D_INTEGRATION should be defined
+
+ Features and Limitations:
+ - Scale and Skew properties are ignored.
+ - Position and rotation are going to updated from the physics body
+ - If you update the rotation or position manually, the physics body will be updated
+ - You can't enble both Chipmunk support and Box2d support at the same time. Only one can be enabled at compile time
+ */
+class LFSpriteNode : public cocos2d::Sprite
+{
+public:
+
+    static LFSpriteNode* create();
+    /** Creates an sprite with a texture.
+     The rect used will be the size of the texture.
+     The offset will be (0,0).
+     */
+    static LFSpriteNode* createWithTexture(cocos2d::Texture2D *pTexture);
+
+    /** Creates an sprite with a texture and a rect.
+     The offset will be (0,0).
+     */
+    static LFSpriteNode* createWithTexture(cocos2d::Texture2D *pTexture, const cocos2d::Rect& rect);
+
+    /** Creates an sprite with an sprite frame. */
+    static LFSpriteNode* createWithSpriteFrame(cocos2d::SpriteFrame *pSpriteFrame);
+
+    /** Creates an sprite with an sprite frame name.
+     An SpriteFrame will be fetched from the SpriteFrameCache by name.
+     If the SpriteFrame doesn't exist it will raise an exception.
+     @since v0.9
+     */
+    static LFSpriteNode* createWithSpriteFrameName(const char *pszSpriteFrameName);
+
+    /** Creates an sprite with an image filename.
+     The rect used will be the size of the image.
+     The offset will be (0,0).
+     */
+    static LFSpriteNode* create(const char *pszFileName);
+
+    /** Creates an sprite with an image filename and a rect.
+     The offset will be (0,0).
+     */
+    static LFSpriteNode* create(const char *pszFileName, const cocos2d::Rect& rect);
+
+    LFSpriteNode();
+
+    virtual bool isDirty() const;
+
+    /** Keep the sprite's rotation separate from the body. */
+    bool isIgnoreBodyRotation() const;
+    void setIgnoreBodyRotation(bool bIgnoreBodyRotation);
+
+    //
+    // Box2d specific
+    //
+    /** Body accessor when using box2d */
+    b2Body* getB2Body() const;
+    void setB2Body(b2Body *pBody);
+
+    float getPTMRatio() const;
+    void setPTMRatio(float fPTMRatio);
+
+    // overrides
+    virtual const cocos2d::Point& getPosition() const override;
+    virtual void getPosition(float* x, float* y) const override;
+    virtual float getPositionX() const override;
+    virtual float getPositionY() const override;
+    virtual void setPosition(const cocos2d::Point &position) override;
+    virtual float getRotation() const override;
+    virtual void setRotation(float fRotation) override;
+    virtual const kmMat4& getNodeToParentTransform() const override;
+    virtual void visit(cocos2d::Renderer *renderer, const kmMat4& parentTransform, bool parentTransformUpdated) override;
+
+protected:
+    const cocos2d::Point& getPosFromPhysics() const;
+
+    bool    _ignoreBodyRotation;
+
+    // box2d specific
+    b2Body  *_pB2Body;
+    float   _PTMRatio;
+};
+
+
+#endif // __LFSPRITENODE_H__
