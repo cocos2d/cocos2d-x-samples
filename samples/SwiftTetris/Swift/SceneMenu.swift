de40f4d74563041933dde004bd19a3d3a9272787
mannewalis <justinagraham@gmail.com>
2014-07-07 18:10:20 -0700

    work

--- samples/SwiftTetris/Swift/SceneMenu.swift
+++ samples/SwiftTetris/Swift/SceneMenu.swift
@@ -31,7 +31,16 @@ class SceneMenu : Scene
         var label = Label.createWithTTF("Play", "edunline.ttf", 150)
         label.setAnchorPoint(CGPointMake(0.5, 0.5))
         label.setPosition(CGPointMake(0.5 * size.width, 700.0))
+        var black = Color4B.createWithBlack() as Color4B // wtf?
+        label.enableShadow(black, CGSizeMake(20, -20), 50)
+        label.enableOutline(black, 4)
         addChild(label)
+
+        var t : Float = 4
+        var action = ClosureAction.createWithDuration(t, { (time : Float) -> Void in
+            label.enableShadow(black, CGSizeMake(CGFloat(time * 20.0), CGFloat(time * -20.0)), 50)
+        })        
+        label.runAction(action)
             
         var listener = EventListenerTouchOneByOne()
         listener.onTouchBegan = startGame
